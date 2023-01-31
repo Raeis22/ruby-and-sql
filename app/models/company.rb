8b485430cd32f6b004ec19b34146eda0503193c0
Raeis22 <ruixianliu2024@u.northwestern.edu>
2023-01-31 02:01:23 +0000

    2 lesson

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
     # responsible for talking to the companies table in the database
+    # first letter of the model name should always be capitalized
 end
--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -15,13 +15,69 @@ Company.destroy_all
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
+puts "there are #{Company.all.count} companies"
+
+company = Company.new
+# puts company.inspect
+company["name"] = "Apple"
+company["city"] = "Cupertino"
+company["state"] = "CA"
+company["url"] = "http://apple.com"
+# puts company.inspecy
+company.save
+# insert into companies () values ()
+
+company = Company.new
+# puts company.inspect
+company["name"] = "Amazon"
+company["city"] = "Seattle"
+company["state"] = "WA"
+company["url"] = "http://amazon.com"
+# puts company.inspecy
+company.save
+# insert into companies () values ()
+
+company = Company.new
+# puts company.inspect
+company["name"] = "Twitter"
+company["city"] = "San francisco"
+company["state"] = "CA"
+company["url"] = "http://twitter.com"
+# puts company.inspecy
+company.save
+# insert into companies () values ()
 
 # 3. query companies table to find all row with California company
+all_companies = Company.all
+puts all_companies.inspect
+puts all_companies[0]#.inspect
+# if do it without .inspect, computer won't give you all the information
+
+cali_companies = Company.where({"state" => "CA"})
+puts cali_companies.inspect
 
 # 4. query companies table to find single row for Apple
+apple = Company.where({"name" => "Apple"})[0]
+apple = Company.find_by({"name" => "Apple"})
+puts apple.inspect
 
 # 5. read a row's column value
+puts apple["url"]
+# or
+puts apple.url
 
 # 6. update a row's column value
+amazon = Company.find_by({"name" => "Amazon"})
+puts amazon.inspect
+
+amazon["url"] = "http://amazon.com"
+amazon.save
+
+puts amazon.inspect
 
 # 7. delete a row
+twitter = Company.find_by({"name" => "Twitter"})
+puts twitter.inspect
+twitter.destroy
+
+puts "there are #{Company.all.count} companies"
